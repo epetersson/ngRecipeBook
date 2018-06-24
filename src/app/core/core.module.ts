d3c7734635d3df531ce604ca81d0f2f2cc83eb7e
grasshopper <emil.petersson@live.com>
2018-06-25 00:04:56 +0200

    Implemented a core module and moved header component and home component

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import { AppRoutingModule } from './../app-routing.module';
+import { SharedModule } from './../shared/shared.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NgModule } from '@angular/core';
+
+// Export header component since its' selector is used in
+// app component html
+@NgModule({
+    declarations: [
+        HeaderComponent,
+        HomeComponent
+    ],
+    imports: [
+        SharedModule,
+        AppRoutingModule
+    ],
+    exports: [
+        AppRoutingModule,
+        HeaderComponent
+    ]
+})
+export class CoreModule {}
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,30 @@
+import { AuthService } from '../../auth/auth.service';
+import { DataStorageService } from '../../shared/data-storage.service';
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+    constructor (private dataStorageService: DataStorageService,
+                 private authService: AuthService) {}
+
+    onSaveData() {
+        this.dataStorageService.storeRecipes()
+            .subscribe(
+                (response: Response) => {
+                    console.log(response);
+                }
+            );
+    }
+
+    onFetchData() {
+        this.dataStorageService.getRecipes();
+    }
+
+    onSignOutUser() {
+        this.authService.signOutUser();
+    }
+}
