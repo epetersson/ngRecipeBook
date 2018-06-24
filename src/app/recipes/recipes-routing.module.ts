dd1b01d004d3fed7ef8a80a7159fadc3a09c0cc7
grasshopper <emil.petersson@live.com>
2018-06-24 21:01:00 +0200

    Created a feature module for the Recipes feature

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,31 @@
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { AuthGuard } from './../auth/auth-guard.service';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesComponent } from './recipes.component';
+import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+const recipesRoutes: Routes = [
+    {
+        path: 'recipes' ,
+        component: RecipesComponent,
+        children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
+            { path: ':id', component: RecipeDetailComponent },
+            { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
+        ]
+    }
+];
+
+// Use forChild() since this is a childmodule that gets
+// imported to app.module.ts
+@NgModule({
+    imports: [RouterModule.forChild(recipesRoutes)],
+    exports: [RouterModule]
+})
+export class RecipesRoutingModule {
+
+}
