ca67c726817e9ac7750a5bfedff0b129779e57ce
Emil Petersson <emil.petersson@live.com>
2018-08-06 14:09:03 +0200

    fixed bug where fetch recipes from db was actually saving them

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -6,7 +6,7 @@ export const ADD_RECIPE = "ADD_RECIPE";
 export const UPDATE_RECIPE = "UPDATE_RECIPE";
 export const DELETE_RECIPE = "DELETE_RECIPE";
 export const STORE_RECIPES = "STORE_RECIPES";
-export const FETCH_RECIPES = "STORE_RECIPES";
+export const FETCH_RECIPES = "FETCH_RECIPES";
 
 export class SetRecipes implements Action {
     readonly type = SET_RECIPES;
